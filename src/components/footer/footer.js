dc1b9fea74c8d5f2ae3d22f9cd6d74e50c3bab26
Nishant nayak <nayaknishant656@gmail.com>
2024-05-26 20:45:06 +0530

    push

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import "./footer.css"
 function footer() {
   return (
     <div className='main-parent'>
-     <p> if i have not been Software Developer i WOuld have Film-maker, you can check out amazing shot on my instagram :)</p><br></br><br></br>
+     <p> if i have not been Software Developer i WOuld have Film-maker, you can check out amazing shot on my instagram :)</p><br></br>
      <p>design and code by nishant</p>
     </div>
   )
