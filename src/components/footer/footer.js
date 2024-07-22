e197c9f315506c32dfb84212ae7cf602e05e66f9
Nishant nayak <nayaknishant656@gmail.com>
2024-07-23 00:40:00 +0530

    thik hai yaar

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,10 +4,10 @@ function footer() {
   return (
     <>
      <div className='main-parent'>
-     <p className='main-footer_txt_center'> if i have not been Software Developer i WOuld have Film-maker, you can check out amazing shot on my instagram :)</p>
-     <br></br><p>design and build by nishant</p>
+     {/* <p className='main-footer_txt_center'> if i have not been Software Developer i WOuld have Film-maker, you can check out amazing shot on my instagram :)</p> */}
+     <p>design and build by nishant</p>
+     <p>console.log("UNDER DEVELOPMENT PHASE")</p>
     </div>
-  
     </>
   )
 }
