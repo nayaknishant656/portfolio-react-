1f65be2a320b0a91f55fd725c6e96c22af2fd574
Nishant nayak <109619832+nayaknishant656@users.noreply.github.com>
2024-11-05 23:11:03 +0530

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,9 +4,7 @@ function footer() {
   return (
     <>
      <div className='main-parent'>
-     {/* <p className='main-footer_txt_center'> if i have not been Software Developer i WOuld have Film-maker, you can check out amazing shot on my instagram :)</p> */}
      <p>design and build by nishant</p>
-     <p>console.log("UNDER DEVELOPMENT PHASE")</p>
     </div>
     </>
   )
