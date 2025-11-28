9d1850fc843022645729b1ce34a75e2cc0a7a708
nayaknishant656 <nayaknishant656@gmail.com>
2025-11-28 16:36:06 +0530

    push the code to the production

--- src/components/blogs/blogs.jsx
+++ src/components/blogs/blogs.jsx
@@ -7,16 +7,16 @@ export default function Blogs() {
       <div className="grandparent_mainheader_parent">
         <div className="parent_mainheader_child">
           <div className="blog_main_grandparent">
+            <h1>Tech Blogs</h1>
             <div className="blog_main_parent">
               <div className="blog_main_head">
                 <Blogcard />
-                  <Blogcard />
-                    <Blogcard />
-                      <Blogcard />
-                        <Blogcard />
-                          <Blogcard />
-                            <Blogcard />
-                            
+                <Blogcard />
+                <Blogcard />
+                <Blogcard />
+                <Blogcard />
+                <Blogcard />
+                <Blogcard />
               </div>
             </div>
           </div>
