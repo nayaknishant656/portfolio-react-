64d9e22c8aaa90d0e5789ae40420748a854913cf
nayaknishant656 <nayaknishant656@gmail.com>
2025-12-21 02:05:52 +0530

    added a paragraph

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -34,6 +34,7 @@ export default function main() {
                 <p>COntribtuion it should i have contributed to the github profile</p>
                 <p>contribution has shown into my zccount lets check this this side of contribution to this side</p>
                 <p>it should added to the github</p>
+                <p>gadi mai dalo isko</p>
               </div>
 
 
