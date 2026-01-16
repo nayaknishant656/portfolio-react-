a505f68d2fd94ed22c24fef5d9840fc516ddfc7a
nayaknishant656 <nayaknishant656@gmail.com>
2026-01-16 21:50:51 +0530

    changed the header file

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,38 +3,38 @@ import Link from 'next/link'
 
 type Props = {}
 
-function Header({}: Props) {
+function Header({ }: Props) {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <nav className="container mx-auto px-4 py-4">
         <ul className="flex items-center justify-center gap-8 flex-wrap">
           <li>
-            <Link 
-              href="/" 
+            <Link
+              href="/"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors duration-200 relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"
             >
-              Homeeee
+              Homeeeeeee
             </Link>
           </li>
           <li>
-            <Link 
-              href="/dashboard" 
+            <Link
+              href="/dashboard"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors duration-200 relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"
             >
               Dashboard
             </Link>
           </li>
           <li>
-            <Link 
-              href="/services" 
+            <Link
+              href="/services"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors duration-200 relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"
             >
               Services
             </Link>
           </li>
           <li>
-            <Link 
-              href="/contact" 
+            <Link
+              href="/contact"
               className="text-gray-700 hover:text-blue-600 font-medium transition-colors duration-200 relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"
             >
               Contact
