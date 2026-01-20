70b21250289301bd5dea369a05ef2a194eb2015c
nayaknishant656 <nayaknishant656@gmail.com>
2026-01-20 22:37:32 +0530

    add some featured on portfolio

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./body.css"
 import Relative from "./relativepath"
 import data from "../../db.json"
+import { FaGithub, FaEnvelope, FaFileAlt } from "react-icons/fa";
 // import Footer from "../footer/footer"
 
 export default function body() {
@@ -12,6 +13,17 @@ export default function body() {
         <div className='header-main '>
           <h2 className="text-3xl font-bold">Hey,Nishant this side</h2>
           <h4>Software Developer/Product Designer</h4>
+          <div className='social-icons flex space-x-4 '>
+            <a href="https://github.com/nayaknishant656" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="social-icon" />
+            </a>
+            <a href="mailto:nishantkumarnayak094@gmail.com">
+              <FaEnvelope className="social-icon" />
+            </a>
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              <FaFileAlt className="social-icon" />
+            </a>
+          </div>
         </div>
         {/* <div className='para-head p-4 '>
           i am 18 year old high school graduate, Building Stuff since 2020 <br></br>
--- src/components/body/relativepath.js
+++ src/components/body/relativepath.js
@@ -4,11 +4,16 @@ import { FaBeer } from 'react-icons/fa';
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function relativepath({ item }) {
-  const { id, title, des, tech, keypoints, img, date, feat, link } = item;
+  const { id, title, des, tech, keypoints, img, date, feat, link, upcoming } = item;
   return (
     <div className='para'>
       {/* <h1 >{title}</h1> */}
       <a className="link-main" href={img}><h1 className='title-heading'>{title}</h1><FaExternalLinkAlt /></a>
+      {upcoming ? (
+        <h2 className='upcoming-project' style={{ color: 'red', fontWeight: 'plain' }}>
+          Upcoming Project
+        </h2>
+      ) : null}
       <h2 className='date-portfolio'>{date}
       </h2>
       {link ? (
@@ -29,7 +34,7 @@ export default function relativepath({ item }) {
             ))}
           </ul>
         </div>
-        <p>Tech Stach :- React, Html, css, javascript,</p>
+        <p>{tech}</p>
       </div>
     </div>
   )
