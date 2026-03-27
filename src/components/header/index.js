854213a2d0c7ea399bd3511650dcc7fa54854d76
nayaknishant656 <nayaknishant656@gmail.com>
2026-03-27 21:06:50 +0530

    corrected Content Grammer spelling and many more along with that

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,30 +2,30 @@ import React from 'react'
 import "./index.css"
 import { Link } from "react-router-dom";
 export default function index() {
-  return (
-    <>
-    <main>
-        <div className='header-parent'>
-            {/* <div className='left-parent'>
+    return (
+        <>
+            <main>
+                <div className='header-parent'>
+                    {/* <div className='left-parent'>
                 <img className='img-parent'></img>
             </div> */}
-            <div className='right-parent'>
-                <nav>
-                    <ul className='ul-flex'>
-                    <Link to="/" className="ul-link"><div>Home</div></Link>
-                    <Link to="Body" className="ul-link"><div>Product</div></Link>
-                    <Link to="Twitter" className="ul-link"><div>Hot Code</div></Link>
-                    <Link to="Source" className="ul-link"><div>Source</div></Link>
-                    <Link to="Blogs" className="ul-link"><div>Blogs</div></Link>
-                    {/* <Link to="Gallery" className="ul-link"><div>Gallery</div></Link>
+                    <div className='right-parent'>
+                        <nav>
+                            <ul className='ul-flex'>
+                                <Link to="/" className="ul-link">Home</Link>
+                                <Link to="Body" className="ul-link">Product</Link>
+                                <Link to="Twitter" className="ul-link">Hot Code</Link>
+                                <Link to="Source" className="ul-link">Source</Link>
+                                <Link to="Blogs" className="ul-link">Blogs</Link>
+                                {/* <Link to="Gallery" className="ul-link"><div>Gallery</div></Link>
                     <Link to="videos" className="ul-link"><div>Videos</div></Link>
                     <Link to="Youtubeblog" className="ul-link"><div>YoutubeBlog</div></Link> */}
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </main>
-    
-    </>
-  )
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </main>
+
+        </>
+    )
 }
